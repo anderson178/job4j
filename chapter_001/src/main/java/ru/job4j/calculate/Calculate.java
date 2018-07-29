000278fbf4485c1e6286bfc4c46c5a658c6c5a59
anderson178 <denismironenko@yandex.ru>
2018-07-29 12:12:28 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,10 @@
 package ru.job4j.calculate;
-
+	/**
+	 *clas Calculate
+	 *@author Denis Mironenko
+	 *@since 29.07.2018
+	 *@version 1.0
+	*/
 public class Calculate
 {
 	public static void main (String[] args )
