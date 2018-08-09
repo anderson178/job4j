eda3001e10d78c4ad1c65ed078d49afd27957ba9
anderson178 <denismironenko@yandex.ru>
2018-08-09 11:13:10 +0300

    6.3. Массив заполнен true или false [#53857]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 09.08.2018
+ */
+
+public class Check {
+    /**
+     * метод проверяет, что все элементы в массиве являются true или false.
+     * @param mas - входящий массив состоящий из true и false.
+     * @return - возвращает true если все элементы массива равны одному состоянию иначе вернет false.
+     */
+    public boolean mono(boolean[] mas) {
+        boolean temp = true;
+        boolean temprtrn = true;
+        for (int i = 0; i < mas.length-1; i++) {
+            temp = mas[i];
+            if (mas[i+1] == temp) {
+                temp = mas[i+1];
+                temprtrn = true;
+            } else {
+                temprtrn = false;
+                break;
+            }
+        }
+        return temprtrn;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 09.08.2018
+ */
+
+public class CheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalseEvenMas() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDataMonoByTrueThenTrueEvenMas() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {false, false, false, false};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+}
