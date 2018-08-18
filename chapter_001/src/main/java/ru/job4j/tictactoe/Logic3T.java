465a6f55b307fc9e63811378a166d133e9927bc6
anderson178 <denismironenko@yandex.ru>
2018-08-18 14:51:26 +0300

    Задание данное при прихождении устного теста

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -75,6 +75,65 @@ public class Logic3T {
 
     public boolean isWinnerO() {
         boolean result = false;
+        int countEnd = this.table.length - 1;
+        //проверяем строки
+        int countCheck = 0;
+        for (int i = 0; i < this.table.length; i++) {
+            for (int j = 0; j < this.table.length; j++) {
+                if (!this.table[i][j].hasMarkX()) {
+                    countCheck++;
+                    if (countCheck == this.table.length) {
+                        result = true;
+                        break;
+                    }
+                } else {
+                    j = this.table.length;
+                }
+            }
+            countCheck = 0;
+        }
+        //проверяем столбцы
+        for (int j = 0; j < this.table.length; j++) {
+            for (int i = 0; i < this.table.length; i++) {
+                if (!this.table[i][j].hasMarkX()) {
+                    countCheck++;
+                    if (countCheck == this.table.length) {
+                        result = true;
+                        break;
+                    }
+                } else {
+                    i = this.table.length;
+                }
+            }
+            countCheck = 0;
+        }
+
+        //прямая диагональ
+        for (int i = 0; i < this.table.length; i++) {
+            if (!this.table[i][i].hasMarkX()) {
+                countCheck++;
+                if (countCheck == this.table.length) {
+                    result = true;
+                    break;
+                }
+            } else {
+                i = this.table.length;
+            }
+            countCheck = 0;
+        }
+        //обратная диагональ
+        for (int i = 0; i < this.table.length; i++) {
+            if (!this.table[i][countEnd].hasMarkX()) {
+                countCheck++;
+                if (countCheck == this.table.length) {
+                    result = true;
+                    break;
+                }
+            } else {
+                i = this.table.length;
+            }
+            countEnd--;
+        }
 
         return result;
 
--- chapter_001/src/test/java/ru/job4j/tictactoe/TictactoeTest.java
+++ chapter_001/src/test/java/ru/job4j/tictactoe/TictactoeTest.java
@@ -20,12 +20,12 @@ public class TictactoeTest {
     @Test
     public void whenHasOWinner() {
         Figure3T[][] table = {
-                {new Figure3T(), new Figure3T(), new Figure3T()},
                 {new Figure3T(false), new Figure3T(true), new Figure3T()},
-                {new Figure3T(false), new Figure3T(false), new Figure3T(false)},
+                {new Figure3T(false), new Figure3T(true), new Figure3T()},
+                {new Figure3T(true), new Figure3T(false), new Figure3T(true)},
         };
         Logic3T login = new Logic3T(table);
-        assertThat(login.isWinnerO(), is(true));
+        assertThat(login.isWinnerO(), is(false));
     }
 
     @Test
