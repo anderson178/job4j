93023e6d8d22104c23626c2896ef4f326313c56f
anderson178 <denismironenko@yandex.ru>
2018-08-19 15:56:20 +0300

    SmartProgress. Calculate matrix on a vector. Task #2

---
+++ SmartProgress/src/main/java/tasks/CalculateMatrix.java
@@ -0,0 +1,27 @@
+package tasks;
+
+/**
+ * @author Denis Mironenko
+ * @version $Id$
+ * @since 19.08.2018
+ */
+public class CalculateMatrix {
+    /**
+     * Метод перемножения матрицы на вектор
+     * @param matrix - входящий двухмерный массив
+     * @param vector - входящий вектор
+     * @return - результат перемножения матрицы на веторк
+     */
+    public int[] calculateMatrixOnVector (int matrix[][], int[] vector) {
+        int[] result = new int[vector.length];
+        int temp = 0;
+        for (int i = 0; i < vector.length; i++) {
+            for (int j =0; j <vector.length; j++) {
+                temp += matrix[i][j] * vector[j];
+            }
+            result[i] = temp;
+            temp = 0;
+        }
+        return result;
+    }
+}
---
+++ SmartProgress/src/test/java/tasks/CalculateMatrixTest.java
@@ -0,0 +1,25 @@
+package tasks;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Denis Mironenko
+ * @version $Id$
+ * @since 19.08.2018
+ */
+public class CalculateMatrixTest {
+    @Test
+    public void CalculateMatrixOnVectorTest() {
+        CalculateMatrix calculate = new CalculateMatrix();
+        int[][] matrix = {
+                {2, 4, 0},
+                {-2, 1, 3},
+                {-1, 0, 1}
+        };
+        int[] vector = {1, 2, -1};
+        int[] expect = {10, -3, -2};
+        int[] result = calculate.calculateMatrixOnVector(matrix, vector);
+        assertThat(result, is(expect));
+    }
+}
