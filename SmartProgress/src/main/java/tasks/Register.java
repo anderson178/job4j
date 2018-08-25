a7fd6aab6b8168b1541b8d5431119e8e2eeb2a4b
anderson178 <denismironenko@yandex.ru>
2018-08-25 12:18:04 +0300

    Smart Progress. Задание 4.1 Приведение строки в нормальный регистр

---
+++ SmartProgress/src/main/java/tasks/Register.java
@@ -0,0 +1,36 @@
+package tasks;
+import java.lang.*;
+import org.apache.commons.lang3.*;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 25.08.2018
+ */
+
+public class Register {
+	/**
+	 * The method of converting a string to a regular register
+	 * @param line - incoming line
+	 * @return
+	 */
+	public String normaslRegister(String line) {
+		String[] splitLine = line.split(" ");
+		String result = "";
+		Character[] tempCharArray;
+		for (int i = 0; i < splitLine.length; i++) {
+			tempCharArray = ArrayUtils.toObject(splitLine[i].toCharArray());
+			for (int j = 0; j < tempCharArray.length; j++) {
+				if (j == 0 ) {
+					result += Character.toUpperCase(tempCharArray[j]);
+				} else {
+					result += Character.toLowerCase(tempCharArray[j]);
+				}
+			}
+			if (i != splitLine.length-1) {
+				result += " ";
+			}
+		}
+		return result;
+	}
+}
---
+++ SmartProgress/src/test/java/tasks/RegisterTest.java
@@ -0,0 +1,21 @@
+package tasks;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 25.08.2018
+ */
+
+public class RegisterTest {
+    @Test
+    public  void normalRegisterWhenLine() {
+        String temp = "ИвАНОВ иВАн иВАНоВич";
+        Register register = new Register();
+        String result = register.normaslRegister(temp);
+        assertThat(result, is ("Иванов Иван Иванович"));
+    }
+}
