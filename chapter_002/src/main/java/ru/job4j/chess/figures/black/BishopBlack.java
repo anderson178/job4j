39e4cc7016ba689689feee7345f76c74796fbcad
anderson178 <denismironenko@yandex.ru>
2018-09-20 22:01:56 +0300

    Шахматы. Слон

--- chapter_002/src/main/java/ru/job4j/chess/figures/black/BishopBlack.java
+++ chapter_002/src/main/java/ru/job4j/chess/figures/black/BishopBlack.java
@@ -1,15 +1,15 @@
 package ru.job4j.chess.figures.black;
 
-import ru.job4j.chess.Chess;
+
 import ru.job4j.chess.exceptionChess.ImpossibleMoveException;
-import ru.job4j.chess.Logic;
+
 import ru.job4j.chess.figures.Cell;
 import ru.job4j.chess.figures.Figure;
 
 /**
  * @author Денис Мироненко
  * @version $Id$
- * @since 13.09.2018
+ * @since 20.09.2018
  */
 
 /**
@@ -32,15 +32,20 @@ public class BishopBlack implements Figure {
     @Override
     public Cell[] way(Cell source, Cell dest, Figure[] figure) throws ImpossibleMoveException {
         Cell[] steps = new Cell[Math.abs(source.y - dest.y)];
-        int wayX = source.x - dest.y;
-        int wayY = source.y - dest.y;
         int deltaY = Math.abs(source.y - dest.y);
         int deltaX = Math.abs(source.x - dest.x);
-        int delta = Math.abs(source.y - dest.y);
-        int numberDest = dest.ordinal();
+        int x1 = 1;
+        int x2 = - 1;
+        int y1 = 1;
+        int y2 = - 1;
         //проверяем может ли фигура так в принципе ходить
-        if (deltaY == deltaX ){
-            steps = new Cell[] {dest};
+        if (deltaY == deltaX) {
+            for (int i = 0; i < steps.length; i++) {
+                int stepX = source.x >= dest.x ? x2-- : x1++;
+                int stepY = source.y >= dest.y ? y2-- : y1++;
+                Cell temp = this.findPosition(source.x + stepX, source.y + stepY);
+                steps[i] = temp;
+           }
         } else {
             throw new ImpossibleMoveException("Нарушение логики хода фигуры");
         }
@@ -50,5 +55,16 @@ public class BishopBlack implements Figure {
     @Override
     public Figure copy(Cell dest) {
         return new BishopBlack(dest);
+    }
+    public Cell findPosition( int x, int y) {
+        Cell[] temp = Cell.values();
+        Cell result = null;
+        for (Cell cell : temp) {
+            if (x == cell.x && y == cell.y) {
+                result = cell;
+                break;
+            }
+        }
+        return result;
     }
 }
