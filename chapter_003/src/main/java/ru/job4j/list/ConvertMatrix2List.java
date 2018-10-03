caa2203a5da6db4f23176f175100709b317ae63e
anderson178 <denismironenko@yandex.ru>
2018-10-03 14:29:30 +0300

    Collections Framework. 3. Конвертация двумерного массива в ArrayList [#48580]

---
+++ chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -0,0 +1,28 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 03.10.2018
+ */
+public class ConvertMatrix2List {
+    /**
+     * метод перевода двухмерного массива в список
+     *
+     * @param array - двухмерный массив
+     * @return - список
+     */
+    public List<Integer> toList(int[][] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int[] external : array) {
+            for (int interrior : external) {
+                list.add(interrior);
+            }
+        }
+        return list;
+    }
+
+}
---
+++ chapter_003/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 03.10.2018
+ */
+
+public class ConvertMatrix2ListTest {
+    @Test
+    public void when2on2ArrayThenList4() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2},
+                {3, 4}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when2on3ArrayThenList6() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4, 5, 6
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when3on3ArrayThenList9() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4, 5, 6, 7, 8, 9
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+}
