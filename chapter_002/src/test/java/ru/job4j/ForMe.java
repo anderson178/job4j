76022163fcf2923b2d9d4889d4f13986655c5957
anderson178 <denismironenko@yandex.ru>
2018-10-06 13:57:14 +0300

    Свои теста

--- chapter_002/src/test/java/ru/job4j/ForMe.java
+++ chapter_002/src/test/java/ru/job4j/ForMe.java
@@ -3,7 +3,7 @@ package ru.job4j;
 public class ForMe {
     int v;
     int p;
-    public ForMe (int v, int p) {
+    public ForMe(int v, int p) {
         this.v = v;
         this.p = p;
     }
@@ -14,12 +14,15 @@ public class ForMe {
     }
 
     public String summ(int a, int b, int d) {
-        String sd = String.valueOf(a + b +d);
+        String sd = String.valueOf(a + b + d);
         return sd;
     }
+   static int sd() {
+        return 1 + 2;
+   }
 
     public static void main(String[] args) {
-        ForMe fm = new ForMe(2,7);
+        ForMe fm = new ForMe(2, 7);
         String f = fm.summ(2, 3, 4);
         System.out.println(f);
     }
---
+++ chapter_002/src/test/java/ru/job4j/ForYou.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+
+public class ForYou extends ForMe {
+    private int a, b;
+    public ForYou(int a, int b) {
+        super(a, b);
+        //this.a = a;
+        //this.b = b;
+
+    }
+    public int qwerty() {
+        //super.summ();
+        return 2 + 2;
+
+    }
+
+    public static void main(String[] args) {
+        ForMe fm = new ForMe(2, 3);
+        //fm.
+
+        System.out.println(ForMe.sd());
+    }
+
+}
