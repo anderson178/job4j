515899a3acad4cfa30bf16c636fa9474bb6d0e26
anderson178 <denismironenko@yandex.ru>
2018-10-11 17:58:03 +0300

    Collections. Lite 1. 3. Компаратор для строк. [#35008]

---
+++ chapter_003/src/main/java/ru/job4j/sortnumbertwo/ListCompare.java
@@ -0,0 +1,27 @@
+package ru.job4j.sortnumbertwo;
+
+import java.util.Comparator;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 11.10.2018
+ */
+public class ListCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int sizeLeft = left.toCharArray().length;
+        int sizeRight = right.toCharArray().length;
+        int size = sizeLeft < sizeRight ? sizeLeft : sizeRight;
+        int result = 0;
+        for (int i = 0; i < size; i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                result = left.charAt(i) < right.charAt(i) ? 1 : -1;
+                break;
+            } else if (i == size - 1 && sizeLeft != sizeRight) {
+                result = sizeLeft < sizeRight ? 1 : -1;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/sort/ListCompareTest.java
@@ -0,0 +1,67 @@
+package ru.job4j.sort;
+
+import org.junit.Test;
+import ru.job4j.sortnumbertwo.ListCompare;
+
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Денис Мироненко
+ * @version $Id$
+ * @since 11.10.2018
+ */
+public class ListCompareTest {
+
+    @Test
+    public void whenStringsAreEqualThenZero() {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanov"
+        );
+        assertThat(rst, is(0));
+    }
+
+    @Test
+    public void whenStringsAreEqualThenPositive() {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Avanov",
+                "Ivanov"
+        );
+        assertThat(rst, is(1));
+    }
+
+    @Test
+    public void whenStringsAreEqualThenPositive2() {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Iva",
+                "Ivana"
+        );
+        assertThat(rst, is(1));
+    }
+
+    @Test
+    public void whenStringsAreEqualThenNegative() {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Patrov"
+        );
+        assertThat(rst, is(-1));
+    }
+
+    @Test
+    public void whenStringsAreEqualThenNegative2() {
+        ListCompare compare = new ListCompare();
+        int rst = compare.compare(
+                "Ivana",
+                "Ivan"
+        );
+        assertThat(rst, is(-1));
+    }
+
+}
