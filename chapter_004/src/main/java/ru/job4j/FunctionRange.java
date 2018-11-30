2b6deba30ad3d08c98a4ecd9f79d3dee95ff0e9d
anderson178 <denismironenko@yandex.ru>
2018-11-30 13:07:23 +0300

    Лямбды. Подсчет функции в диапозоне.

--- chapter_004/src/main/java/ru/job4j/FunctionRange.java
+++ chapter_004/src/main/java/ru/job4j/FunctionRange.java
@@ -9,9 +9,9 @@ import java.util.function.Function;
 
 public class FunctionRange {
 
-    public List<Double> linFunction(int start, int end, Function<Double, Double> func) {
+    public List<Double> function(int start, int end, Function<Double, Double> func) {
         List<Double> rst = new ArrayList<>();
-        for (int x = start; x != end; x ++) {
+        for (int x = start; x != end; x++) {
             rst.add(func.apply(Double.valueOf(x)));
         }
         return rst;
--- chapter_004/src/test/java/ru/job4j/TestFunctionRange.java
+++ chapter_004/src/test/java/ru/job4j/TestFunctionRange.java
@@ -8,10 +8,31 @@ import static org.junit.Assert.assertThat;
 
 public class TestFunctionRange {
     @Test
-    public void whenLinFinction() {
+    public void whenLinFunction() {
         FunctionRange func = new FunctionRange();
-        ArrayList<Double> rst = new ArrayList<>() ;
-        rst.addAll(func.linFunction(1, 10, (x) -> 2 * x + 4));
+        ArrayList<Double> rst = new ArrayList<>();
+        int k = 2, b = 4;
+        rst.addAll(func.function(1, 10, (x) -> k * x + b));
         assertThat(rst, is(Arrays.asList(6D, 8D, 10D, 12D, 14D, 16D, 18D, 20D, 22D)));
+    }
+
+    @Test
+    public void whenQuadrFunction() {
+        FunctionRange func = new FunctionRange();
+        ArrayList<Double> rst = new ArrayList<>();
+        int a = 1, b = 2, c = 3;
+        rst.addAll(func.function(1, 10, (x) -> a * Math.pow(x, 2) + b * x + c));
+        System.out.println(rst);
+        assertThat(rst, is(Arrays.asList(6D, 11D, 18D, 27D, 38D, 51D, 66D, 83D, 102D)));
+    }
+
+    @Test
+    public void whenLogFunction() {
+        FunctionRange func = new FunctionRange();
+        ArrayList<Double> rst = new ArrayList<>();
+        int a = 1, b = 2, c = 3;
+        rst.addAll(func.function(1, 10, (x) -> Math.floor(Math.log(x))));
+        System.out.println(rst);
+        assertThat(rst, is(Arrays.asList(0D, 0D, 1D, 1D, 1D, 1D, 1D, 2D, 2D)));
     }
 }
