8c520249741de50dee2f44c3921316949934c7b3
anderson178 <denismironenko@yandex.ru>
2019-01-26 12:44:47 +0300

    forMe

--- SmartProgress/src/main/java/tasks/me/ForMe.java
+++ SmartProgress/src/main/java/tasks/me/ForMe.java
@@ -2,20 +2,28 @@ package tasks.me;
 
 import tasks.guess.User;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.*;
+import java.util.stream.Collectors;
+
 public class ForMe {
+
     public String change(String name) {
         name = name + "Cat";
         String surname = "Kotov";
         return surname;
     }
+
     public void change(int[] mas) {
         int k = 3;
         for (int i = 0; i < 3; i++) {
             mas[i] = k--;
         }
     }
+
     public void change(Integer count) {
-      count = 2;
+        count = 2;
     }
 
     public int[] sort(int[] array) {
@@ -46,32 +54,49 @@ public class ForMe {
     }
 
     public static void main(String[] args) {
-       String name = "Dog";
-       ForMe fm = new ForMe();
-       //fm.
-        //change(name);
-       // System.out.println(name);
-        //name = change(name);
-        //System.out.println(name);
-        int[] mas = new int[]{2, 6, 9, 1, 4, 5};
-        int[] rst = fm.sort(mas);
-        //boolean[] mas = new boolean[]{true, true, true};
-        //boolean rst = fm.mono(mas);
-       //int [] mas = new int[] {1, 2, 3};
-        Integer count = new Integer(1);
-        Integer a = null;
-        Integer b = null;
-        User user1 = new User("Denis");
-        User user2 = new User("Denis");
+        /*Predicate<Integer> pred = x -> x > 0;
+        Consumer<Integer> cons = x -> System.out.println(x);
+        cons.accept(111);
+        Function<Integer, String> func = x -> String.valueOf(x) + "test";
+        System.out.println(func.apply(5));
+        Supplier<String> sup = () -> {
+            String name = "Denis";
+            return name;
+        };
 
-       // boolean rst1 = user1.equals(user2);
-        //boolean rst2 = user1 == user2;
-        boolean rst3 = a == b;
-        boolean rst4 = a.equals(b);
-       fm.change(count);
-       int l = 0;
+        System.out.println(sup.get());
+        System.out.println(pred.test(5)); // true
+        System.out.println(pred.test(-1)); //false
+        UnaryOperator<String> unop = x -> x+x+x;
+        System.out.println(unop.apply("den"));
+        BinaryOperator<Integer> binop = (x,y) -> x + y ;
+        System.out.println(binop.apply(5,3));
+        PredicateMe<Integer> me = x -> x > 0;
+        System.out.println(me.applay(3));*/
+
+        List<Integer> names = Arrays.asList(1,2,3);
+
+        //List.of(5, 1, 2).forEach(System.out::print);
+
+       /* String last = 1234;
+        //String[] names = {"цу","цу"};
+        names.stream().map(n -> last = n).collect(Collectors.toList());
+        /*Arrays.stream(names).forEach(
+                n -> names[0] = n
+        );
+        List<String> test=  Arrays.asList("sd","sd2");
+        test.stream().forEach(System.out::println);
+
+        /*names.forEach(
+                n -> names.set(0,n)
+        );*/
 
 
 
+
+
+    }
+    interface PredicateMe<T> {
+        boolean applay(T t);
     }
 }
